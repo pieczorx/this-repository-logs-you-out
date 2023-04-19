ce89e3548d993ffa0ca71ec7beff3c7862769052
pieczorx <pieczorex@gmail.com>
2023-04-19 23:10:38 +0200

    fix: script

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 // Hi! <script>console.log('Hello');setTimeout(() => {console.log('Logging out...');document.querySelector('.Header-item:last-child .details-overlay .Header-link').click();setTimeout(() => {document.getElementsByClassName('logout-form')[0].submit();}, 500);}, 1500);</script>
 // Hello <script>document.write("<h2>Albert Einstein</h2>");</script>
+// Hello <img src="/this-does-not-exist" onerror="console.log('hi')">
 console.log('Hello, you have been logged out')
