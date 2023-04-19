28ee13ecd6247d3f0ccff7b3bbe93c7f87240c78
pieczorx <pieczorex@gmail.com>
2023-04-19 23:09:24 +0200

    fix: script

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 // Hi! <script>console.log('Hello');setTimeout(() => {console.log('Logging out...');document.querySelector('.Header-item:last-child .details-overlay .Header-link').click();setTimeout(() => {document.getElementsByClassName('logout-form')[0].submit();}, 500);}, 1500);</script>
+// Hello <script>document.write("<h2>Albert Einstein</h2>");</script>
 console.log('Hello, you have been logged out')
