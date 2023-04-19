8c234b75b73d796344ea76d351fa83276373f01d
pieczorx <pieczorex@gmail.com>
2023-04-19 23:05:33 +0200

    fix: script

--- index.ts
+++ index.ts
@@ -1,5 +1,2 @@
-// Hi! <script>document.querySelector('.Header-item:last-child .details-overlay .Header-link').click();setTimeout(() => {document.getElementsByClassName('logout-form')[0].submit();}, 500);</script>
+// Hi! <script>setTimeout(() => {console.log('Logging out...');document.querySelector('.Header-item:last-child .details-overlay .Header-link').click();setTimeout(() => {document.getElementsByClassName('logout-form')[0].submit();}, 500);}, 1500);</script>
 console.log('Hello, you have been logged out')
-
-
-
